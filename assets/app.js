889cc5660324f1b289f3f8898bfffe7ea7388cfc
Jenny Valle <jennyvalle@Jennys-MacBook-Air.local>
2018-01-03 20:53:36 -0500

    Started to add rendering

--- assets/app.js
+++ assets/app.js
@@ -6,16 +6,64 @@ const words = ["the quick brown fox jumps over the lazy dog","the lazy dog jumps
 
 let guessed = [];
 
+let chances = 10;
+
+let toGuess = "";
+
+
+// Chooses a new word 
+
+function getWord(){
+
+	return words[Math.floor(Math.random() * 1)];
+
+}
+
+// Renders the word
+
+function renderWord(){
+
+	let display = ""
+	for( let i = 0 ; i < toGuess.length ; i ++ ){
+
+		if(guessed.indexOf(toGuess[i]) === -1 && toGuess[i] !== " "){
+
+			display += "_";
+
+		} else {
+
+			display += toGuess[i];	
+
+		}
+
+	}
+
+	$("#selector").text(display);
+
+}
+
+// Creates new letter buttons
+
 function newLetter(letter){
 
 	return $("<button>").addClass("btn btn-primary").attr("id",letter).text(letter.toUpperCase());
 
 }
 
+// Changes buttons
+
 function dangerLetter(letter){
 
 	$("#" + letter).removeClass("btn-primary").addClass("btn-danger");
 
+}
+
+// Resets all letters to "btn-primary"
+
+function resetLetters(){
+	for(let i = 0 ; i < guessed.length ; i++){
+		$("#" + guessed[i]).removeClass("btn-danger").addClass("btn-primary");
+	}
 }
 
 // Appends all the letters
@@ -25,14 +73,16 @@ for(let i = 0 ; i < alphabet.length ; i++){
 	/* NEED SELECTOR FOR ALPHABET */
 
 	newLetter(alphabet[i]).appendTo("#letter-test");
-	
+
 
 }
 
+toGuess = getWord();
+
 document.addEventListener("keyup",function(e){
 
-	if(/[a-zA-Z]/.test(e.key) && e.key.length === 1){
-		console.log(e.key);
+	if(/[a-zA-Z]/.test(e.key) && e.key.length === 1 && guessed.indexOf(e.key.toLowerCase()) === -1){
+		guessed.push(e.key);
 		dangerLetter(e.key.toLowerCase());
 	}
 
